82657eae9f0d2bb99148cc37a59e67c551830a95
Jabudda <ajafuta@gmail.com>
2025-12-16 16:51:54 -0600

    Update service worker cache version and files

--- sw.js
+++ sw.js
@@ -1,19 +1,25 @@
-const CACHE_NAME = 'success-driver-v1';
+const CACHE_NAME = "success-driver-v2";
 const FILES = [
-  '/',
-  '/index.html',
-  '/manifest.webmanifest',
-  '/sw.js'
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./sw.js",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
-self.addEventListener('install', e => {
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES)));
+});
+
+self.addEventListener("activate", (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
   );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
